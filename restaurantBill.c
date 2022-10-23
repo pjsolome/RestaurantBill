675dff33dc67bafb721be4aed30ad7383af26a62
Parker Solome <pjsolome@cpp.edu>
2022-10-23 13:09:05 -0700

    Finished first version

--- restaurantBill.c
+++ restaurantBill.c
@@ -1,13 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
 
-//Check for correct amount of arguments
+//Meal structure
+typedef struct {
+char *name;
+double price;
+} Meal;
 
-//Randomly choose salad, soup, sandwich, or pizza
+int main(int argc, char *argv[]) {
 
-//Display meal cost
+    //Check for correct amount of arguments
+    if(argc == 3) {
 
-//Display tax amount
+        //Declare and initialize meals
+        Meal salad;
+        Meal soup;
+        Meal sandwich;
+        Meal pizza;
 
-//Display tip amount
+        salad.name = "Salad";
+        salad.price = 9.95;
 
-// Display total bill
+        soup.name = "Soup";
+        soup.price = 4.55;
+
+        sandwich.name = "Sandwich";
+        sandwich.price = 13.25;
+
+        pizza.name = "Pizza";
+        pizza.price = 22.35;
+
+        //Randomly choose meal
+        Meal toBeCalculated;
+        time_t t;
+        int randomNum;
+
+        srand((unsigned) time(&t)); 
+        randomNum = rand() % 4;
+
+        if(randomNum == 0) {
+            toBeCalculated = salad;
+        } else if(randomNum == 1) {
+            toBeCalculated = soup;
+        } else if(randomNum == 2) {
+            toBeCalculated = sandwich;
+        } else {
+            toBeCalculated = pizza;
+        }
+
+        //Display meal cost
+        printf("%s: $%d\n", toBeCalculated.name, toBeCalculated.price);
+
+        //Display tax amount
+        double taxInput = strtod(argv[1], NULL);
+        double taxAmount = (taxInput / 100) * toBeCalculated.price;
+        printf("Tax: $%d\n", taxAmount);
+
+        //Display tip amount
+        double tipInput = strtod(argv[2], NULL);
+        double tipAmount = (tipInput / 100) * toBeCalculated.price;
+        printf("Tip: $%d\n", tipAmount);
+
+        // Display total bill
+        double total = toBeCalculated.price + taxAmount + tipAmount;
+        printf("Total: $%d\n", total);
+        
+        return 0;
+    } else if(argc > 3) {
+        printf("Too many arguments supplied\n");
+        return 1;
+    } else {
+        printf("Two arguments expected\n");
+        return 1;
+    }
+}
