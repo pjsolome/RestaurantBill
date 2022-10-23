d5147d2ae9839c19910d5bd9a2c3375db7897428
Parker Solome <pjsolome@cpp.edu>
2022-10-23 11:43:12 -0700

    Initial commit

---
+++ restaurantBill.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Check for correct amount of arguments
+
+//Randomly choose salad, soup, sandwich, or pizza
+
+//Display meal cost
+
+//Display tax amount
+
+//Display tip amount
+
+// Display total bill
